6f4dcf57323e2c2db746ef63e1a19ba137788f74
olgaoznovich <ol.oznovich@gmail.com>
2025-05-21 17:06:57 +0300

    added first attempt with our word2vec

--- flask4modelcache.py
+++ flask4modelcache.py
@@ -6,6 +6,7 @@ import configparser
 import json
 from modelcache import cache
 from modelcache.adapter import adapter
+from modelcache.embedding.Word2Vec import Word2Vec
 from modelcache.manager import CacheBase, VectorBase, get_data_manager
 from modelcache.similarity_evaluation.distance import SearchDistanceEvaluation
 from modelcache.processor.pre import query_multi_splicing
@@ -57,9 +58,16 @@ data_manager = get_data_manager(CacheBase("mysql", config=mysql_config),
 # data_manager = get_data_manager(CacheBase("mysql", config=mysql_config),
 #                                 VectorBase("redis", dimension=data2vec.dimension, redis_config=redis_config))
 
+###### YO
+USING_WORD2VEC = True
 
+if USING_WORD2VEC:
+    embedding_func = lambda x: Word2Vec().embedding_func(x)
+else:
+    embedding_func = data2vec.to_embeddings
+######
 cache.init(
-    embedding_func=data2vec.to_embeddings,
+    embedding_func=embedding_func,
     data_manager=data_manager,
     similarity_evaluation=SearchDistanceEvaluation(),
     query_pre_embedding_func=query_multi_splicing,
