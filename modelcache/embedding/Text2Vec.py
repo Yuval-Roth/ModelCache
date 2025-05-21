5888a11dd718b6a0a2f4b662be59a3ba5f764bd4
Yuval Roth <rothyuv@post.bgu.ac.il>
2025-05-21 14:59:19 +0300

    Added google's embedding model 'bert_embedder' to support english text Also added some missing dependencies that were required

---
+++ modelcache/embedding/Text2Vec.py
@@ -0,0 +1,30 @@
+###### YO
+import os
+
+from mediapipe.tasks import python
+from mediapipe.tasks.python import text
+
+# model download link:
+# https://storage.googleapis.com/mediapipe-models/text_embedder/bert_embedder/float32/1/bert_embedder.tflite
+
+class Text2Vec:
+    def __init__(self):
+        l2_normalize = True  # @param {type:"boolean"}
+        quantize = False  # @param {type:"boolean"}
+        cwd = os.getcwd()
+        base_options = python.BaseOptions(model_asset_path=  cwd+'/model/embedder.tflite')
+        self.options = text.TextEmbedderOptions(base_options=base_options, l2_normalize=l2_normalize, quantize=quantize)
+        self.dimension = 512
+        pass
+
+    def embedding_func(self, *args, **kwargs):
+        if not args:
+            raise ValueError("No word provided for embedding.")
+        # Use it
+        with text.TextEmbedder.create_from_options(self.options) as embedder:
+            result = embedder.embed(args[0])  # returns a 300-dim vector
+            return result.embeddings[0].embedding
+
+######
+
+
