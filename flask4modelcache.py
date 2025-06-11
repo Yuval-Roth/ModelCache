adf8867053f7697e1993b85d0ffd0a51e98f3e3a
Yuval Roth <rothyuv@post.bgu.ac.il>
2025-06-12 01:51:15 +0300

    New feature: Embedding Dispatcher for parallel embedding Based on the multiprocessing module, provides true parallel embedding. The number of workers can be adjusted in the cache initialization.

--- flask4modelcache.py
+++ flask4modelcache.py
@@ -1,34 +1,50 @@
 # -*- coding: utf-8 -*-
+import asyncio
+
 from flask import Flask, request
 import json
 from modelcache.cache import Cache
-
-# 创建一个Flask实例
-app = Flask(__name__)
-
-cache = Cache.init("mysql","milvus")
-
-@app.route('/welcome')
-def first_flask():  # 视图函数
-    return 'hello, modelcache!'
+from modelcache.embedding import EmbeddingModel
 
 
-@app.route('/modelcache', methods=['GET', 'POST'])
-def user_backend():
-    param_dict = {}
-    try:
-        if request.method == 'POST':
-            param_dict = request.json
-        elif request.method == 'GET':
-            param_dict = request.args
+async def main():
 
-        return json.dumps(cache.handle_request(param_dict))
-    except Exception as e:
-        result = {"errorCode": 101, "errorDesc": str(e), "cacheHit": False, "delta_time": 0, "hit_query": '',
-                  "answer": ''}
-        cache.save_query_resp(result, model='', query='', delta_time=0)
-        return json.dumps(result)
+    # 创建一个Flask实例
+    app = Flask(__name__)
+
+    cache,loop = await Cache.init(
+        sql_storage="mysql",
+        vector_storage="milvus",
+        embedding_model=EmbeddingModel.HUGGINGFACE_ALL_MPNET_BASE_V2,
+        embedding_workers_num=1
+    )
+
+    @app.route('/welcome')
+    def first_flask():  # 视图函数
+        return 'hello, modelcache!'
+
+
+    @app.route('/modelcache', methods=['GET', 'POST'])
+    def user_backend():
+        param_dict = {}
+        try:
+            if request.method == 'POST':
+                param_dict = request.json
+            elif request.method == 'GET':
+                param_dict = request.args
+
+            result = asyncio.run_coroutine_threadsafe(
+                cache.handle_request(param_dict), loop
+            ).result()
+            return json.dumps(result)
+        except Exception as e:
+            result = {"errorCode": 101, "errorDesc": str(e), "cacheHit": False, "delta_time": 0, "hit_query": '',
+                      "answer": ''}
+            cache.save_query_resp(result, model='', query='', delta_time=0)
+            return json.dumps(result)
+
+    await asyncio.to_thread(app.run, host='0.0.0.0', port=5000)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    asyncio.run(main())
